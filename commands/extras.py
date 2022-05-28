93c22ef37680738a26493c5c090a13f512d512eb
visveshkhanna <visveshkhanna@gmail.com>
2022-05-28 22:13:28 +0530

    Updated ping
    1. ping command updated
    * Updated ping command with ping3 module

--- commands/extras.py
+++ commands/extras.py
@@ -3,15 +3,15 @@ import time
 from telegram import Update
 from telegram.ext import CallbackContext
 from handles.extras import *
+from ping3 import ping
+from math import ceil
 
 async def ping(update: Update, context: CallbackContext):
-    start_time = int(round(time.time() * 1000))
     reply = await update.message.reply_text(
         text=italic("Pinging..."),
         parse_mode="HTML"
     )
-    end_time = int(round(time.time() * 1000))
     await reply.edit_text(
-        text=f"{bold('PING: ')} {end_time - start_time} ms",
+        text=f"{bold('PING: ')} {ceil(ping('api.telegram.org') * 100)} ms",
         parse_mode="HTML"
     )
