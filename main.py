31b89967d728e6fd1e2ed0c829c6a1c5be050a39
visveshkhanna <visveshkhanna@gmail.com>
2022-06-07 22:58:11 +0530

    Error handler
    Fixes: )
    Co-Authored-By: lspriya75 <105928953+lspriya75@users.noreply.github.com>

--- main.py
+++ main.py
@@ -40,27 +40,24 @@ def start(update: Update, context: CallbackContext) -> None:
         update.message.reply_text(nonr_message, reply_markup=register_markup, parse_mode="HTML")
 
 
-async def error_handler(update: object, context: CallbackContext) -> None:
+def error_handler(update: object, context: CallbackContext) -> None:
+
     logger.error(msg="Exception while handling an update:", exc_info=context.error)
 
     tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    tb_string = "".join(tb_list)
+    tb_string = ''.join(tb_list)
 
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
     message = (
-        f"An exception was raised while handling an update\n"
-        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-        "</pre>\n\n"
-        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
-        f"<pre>{html.escape(tb_string)}</pre>"
+        f'An exception was raised while handling an update\n'
+        f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
+        '</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
     )
 
-    # Finally, send the message
-    Bot(TOKEN).send_message(
-        chat_id=OWNER, text=message, parse_mode="HTML"
-    )
-
+    context.bot.send_message(chat_id=int(OWNER), text=message, parse_mode="HTML")
 
 def main() -> None:
     updater = Updater(TOKEN)
